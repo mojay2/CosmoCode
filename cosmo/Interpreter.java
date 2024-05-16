970e0b8dfffe83f3a47f2d262845c8038932ecae
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2024-05-16 13:59:14 +0800

    interpreter for assignment

--- cosmo/Interpreter.java
+++ cosmo/Interpreter.java
@@ -19,6 +19,9 @@ public class Interpreter {
             case "decStmt":
                 declaration(root, valueTable);
                 break;
+            case "assignStmt":
+                assignment(root, valueTable);
+                break;
             // Add more cases for other statement types
             default:
                 for (ParseTreeNode child : root.getChildren()) {
@@ -58,11 +61,7 @@ public class Interpreter {
                     }
                     break;
                 case "comet_literal": 
-                    if (getLeafValue(child) == null) {
-                        break;
-                    } else {
-                        value = getLeafValue(child);
-                    }
+                    value = getLeafValue(child);
                     break;  
             }
         }
@@ -82,6 +81,44 @@ public class Interpreter {
                 }
             } else {
                 System.out.println("ERROR: " + identifier + " has already been declared.");
+            }
+        }
+    }
+
+    private static void assignment(ParseTreeNode node, HashMap<String, String> valueTable) {
+        String identifier = null;
+        String value = null;
+    
+        for (ParseTreeNode child : node.getChildren()) {
+            switch (child.getSymbol()) {
+                case "identifier":
+                    if (identifier == null) {
+                        identifier = getLeafValue(child);
+                    } else { // the second identifier encountered is the value (the one after the equal sign)
+                        value = getLeafValue(child); 
+                    }
+                    break;
+                case "comet_literal": 
+                    value = getLeafValue(child);
+                    break;  
+            }
+        }
+    
+        if (identifier != null && value != null) {
+            if (valueTable.containsKey(identifier)) {
+                if (value.matches("-?\\d+(\\.\\d+)?")) {
+                    valueTable.put(identifier, value);
+                    System.out.println("Declaration: " + identifier + " = " + value);
+                    identifier = value;
+                } else if (valueTable.containsKey(value)) {
+                    String assignedValue = valueTable.get(value);
+                    valueTable.put(identifier, assignedValue);
+                    System.out.println("Declaration: " + identifier + " = " + assignedValue);
+                } else {
+                    System.out.println("ERROR: " + value + " has not yet been been declared.");
+                }
+            } else {
+                System.out.println("ERROR: " + identifier + " has not yet been declared.");
             }
         }
     }
