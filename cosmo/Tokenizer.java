0765b3f73450fc6879fddaf3e6bf965429e4ac1b
Hasmire <lagman.jrgreggvictor@gmail.com>
2024-05-17 22:31:32 +0800

    updated flush

--- cosmo/Tokenizer.java
+++ cosmo/Tokenizer.java
@@ -43,6 +43,9 @@ public class Tokenizer {
 
             int choice = scanner.nextInt();
 
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+
             String[] tokens = processInput("./input/input" + choice + ".txt",
                     "./output/tokenizer/output" + choice + ".txt",
                     "./output/symbol_table/output" + choice + ".txt");
